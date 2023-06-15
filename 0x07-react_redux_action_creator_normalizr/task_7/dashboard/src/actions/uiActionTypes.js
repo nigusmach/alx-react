e055f803018aa01c57875a7651e3276322d6a216
nigusmach <nigusmachin@gmail.com>
2023-06-15 07:47:46 -0700

    my firs commit

---
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionTypes.js
@@ -0,0 +1,17 @@
+export const LOGIN = "LOGIN";
+
+export const LOGOUT = "LOGOUT";
+
+export const DISPLAY_NOTIFICATION_DRAWER = "DISPLAY_NOTIFICATION_DRAWER";
+
+export const HIDE_NOTIFICATION_DRAWER = "HIDE_NOTIFICATION_DRAWER";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
